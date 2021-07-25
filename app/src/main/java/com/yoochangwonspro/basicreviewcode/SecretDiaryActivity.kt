094438ff191179d8bec92a117ec7c3128b29f93f
CN <ckddnjs554@naver.com>
2021-07-25 18:27:07 +0900

    Feat : SecretDiaryActivity falsePasswordPopUp 함수 추가 패스워드 불일치시 다이로그 창 팝업 기능

--- app/src/main/java/com/yoochangwonspro/basicreviewcode/SecretDiaryActivity.kt
+++ app/src/main/java/com/yoochangwonspro/basicreviewcode/SecretDiaryActivity.kt
@@ -3,6 +3,8 @@ package com.yoochangwonspro.basicreviewcode
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.NumberPicker
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_secret_diary.*
 
 class SecretDiaryActivity : AppCompatActivity() {
 
@@ -39,5 +41,24 @@ class SecretDiaryActivity : AppCompatActivity() {
         secretNumberPikerOne
         secretNumberPikerTwo
         secretNumberPikerThree
+    }
+
+    private fun initSecretOpenButton() {
+        secret_open_btn.setOnClickListener {
+            if (secretCheckPassword) {
+                // 다이어리 페이지로 이동
+            } else {
+                // 패스워드 불일치일 경우 다이로그 창 띄우기
+            }
+        }
+    }
+
+    private fun falsePasswordPopUp() {
+        AlertDialog.Builder(this)
+            .setTitle("실패!!")
+            .setMessage("비밀번호가 틀렸습니다.")
+            .setPositiveButton("확인") { _, _ -> }
+            .create()
+            .show()
     }
 }
