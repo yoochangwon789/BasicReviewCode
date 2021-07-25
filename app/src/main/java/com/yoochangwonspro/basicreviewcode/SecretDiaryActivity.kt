506f25d92658ef7ea40b124f9c20fa9bff675126
CN <ckddnjs554@naver.com>
2021-07-25 18:13:12 +0900

    Feat : SecretDiaryActivity numberPicker lazy, apply 형태 전역 변수 추가

--- app/src/main/java/com/yoochangwonspro/basicreviewcode/SecretDiaryActivity.kt
+++ app/src/main/java/com/yoochangwonspro/basicreviewcode/SecretDiaryActivity.kt
@@ -2,8 +2,34 @@ package com.yoochangwonspro.basicreviewcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.NumberPicker
 
 class SecretDiaryActivity : AppCompatActivity() {
+
+    private val secretNumberPikerOne: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.secret_number_picker_one)
+            .apply {
+                this.minValue = 1
+                this.maxValue = 45
+            }
+    }
+
+    private val secretNumberPikerTwo: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.secret_number_picker_two)
+            .apply {
+                this.minValue = 1
+                this.maxValue = 45
+            }
+    }
+
+    private val secretNumberPikerThree: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.secret_number_picker_three)
+            .apply {
+                this.minValue = 1
+                this.maxValue = 45
+            }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_secret_diary)
