11c4e44b83870dba9a99135ab4aa9b82a8bb17f7
CN <ckddnjs554@naver.com>
2021-07-26 00:11:34 +0900

    Feat : DetailDiaryActivity Handler 전역 변수 설정

--- app/src/main/java/com/yoochangwonspro/basicreviewcode/DetailDiaryActivity.kt
+++ app/src/main/java/com/yoochangwonspro/basicreviewcode/DetailDiaryActivity.kt
@@ -2,10 +2,18 @@ package com.yoochangwonspro.basicreviewcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import kotlinx.android.synthetic.main.activity_detail_diary.*
 
 class DetailDiaryActivity : AppCompatActivity() {
+
+    private val handler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_diary)
+
+
     }
 }
