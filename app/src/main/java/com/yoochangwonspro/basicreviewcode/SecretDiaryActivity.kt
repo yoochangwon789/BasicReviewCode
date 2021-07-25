119ea13390c2d052582738cda16fb7cfc850ba56
CN <ckddnjs554@naver.com>
2021-07-25 23:45:27 +0900

    Feat : SecretDiaryActivity ChangePassword 버튼 기능 구현

--- app/src/main/java/com/yoochangwonspro/basicreviewcode/SecretDiaryActivity.kt
+++ app/src/main/java/com/yoochangwonspro/basicreviewcode/SecretDiaryActivity.kt
@@ -61,6 +61,32 @@ class SecretDiaryActivity : AppCompatActivity() {
                 falsePasswordPopUp()
             }
         }
+
+        secret_password_change.setOnClickListener {
+
+            if (secretCheckPassword) {
+                sp.edit {
+                    putString("passwordReview", "${secretNumberPikerOne.value}${secretNumberPikerTwo.value}${secretNumberPikerThree.value}")
+                    apply()
+                }
+                secret_password_change.setBackgroundColor(Color.BLACK)
+                Toast.makeText(this, "비밀번호가 변경되었습니다.", Toast.LENGTH_LONG).show()
+                secretCheckPassword = false
+            }
+            else {
+                if (sp.getString("passwordReview", "000").equals(
+                        "${secretNumberPikerOne.value}${secretNumberPikerTwo.value}${secretNumberPikerThree.value}"
+                    )
+                ) {
+                    Toast.makeText(this, "변경할 패스워드를 입력하세요", Toast.LENGTH_LONG).show()
+                    secretCheckPassword = true
+                    secret_password_change.setBackgroundColor(Color.RED)
+                }
+                else {
+                    falsePasswordPopUp()
+                }
+            }
+        }
     }
 
     private fun falsePasswordPopUp() {
