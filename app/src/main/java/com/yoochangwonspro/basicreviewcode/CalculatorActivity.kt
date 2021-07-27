58f75385dbd514295e653458c6083d5bf18e8106
CN <ckddnjs554@naver.com>
2021-07-27 18:25:16 +0900

    Feat : Calculator Button 셋팅 완료

--- app/src/main/java/com/yoochangwonspro/basicreviewcode/CalculatorActivity.kt
+++ app/src/main/java/com/yoochangwonspro/basicreviewcode/CalculatorActivity.kt
@@ -2,10 +2,27 @@ package com.yoochangwonspro.basicreviewcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class CalculatorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
+    }
+
+    fun calculatorClearButton(v: View) {
+
+    }
+
+    fun calculatorHistoryButton(v: View) {
+
+    }
+
+    fun calculatorButton(v: View) {
+
+    }
+
+    fun calculatorResultButton(v: View) {
+
     }
 }
