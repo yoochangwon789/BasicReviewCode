f8fdcbd169069434e170966ca7d7d043e0c366a3
CN <ckddnjs554@naver.com>
2021-07-27 18:37:59 +0900

    Feat : Calculator 전역 변수 초기 셋팅

--- app/src/main/java/com/yoochangwonspro/basicreviewcode/CalculatorActivity.kt
+++ app/src/main/java/com/yoochangwonspro/basicreviewcode/CalculatorActivity.kt
@@ -3,11 +3,29 @@ package com.yoochangwonspro.basicreviewcode
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 
 class CalculatorActivity : AppCompatActivity() {
+
+    private val expressionTextView: TextView by lazy {
+        findViewById(R.id.calculator_expression_text_view)
+    }
+
+    private val resultTextView: TextView by lazy {
+        findViewById(R.id.calculator_result_text_view)
+    }
+
+    private var isOperator: Boolean = false
+
+    private var hasOperator: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
+    }
+
+    fun calculatorButton(v: View) {
+
     }
 
     fun calculatorClearButton(v: View) {
@@ -15,10 +33,6 @@ class CalculatorActivity : AppCompatActivity() {
     }
 
     fun calculatorHistoryButton(v: View) {
-
-    }
-
-    fun calculatorButton(v: View) {
 
     }
 
