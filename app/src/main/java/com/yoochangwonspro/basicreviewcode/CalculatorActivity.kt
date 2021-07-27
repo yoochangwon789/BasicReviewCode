b646f8c9906fd50dc20dbe90a088f1edae1e33c5
CN <ckddnjs554@naver.com>
2021-07-27 18:46:12 +0900

    Feat : Calculator number, operator Button 클릭시 반환 함수 구현

--- app/src/main/java/com/yoochangwonspro/basicreviewcode/CalculatorActivity.kt
+++ app/src/main/java/com/yoochangwonspro/basicreviewcode/CalculatorActivity.kt
@@ -24,19 +24,43 @@ class CalculatorActivity : AppCompatActivity() {
         setContentView(R.layout.activity_calculator)
     }
 
-    fun calculatorButton(v: View) {
+    private fun calculatorButton(v: View) {
+        when (v.id) {
+            R.id.calculator_zero_btn -> calculatorNumberButton("0")
+            R.id.calculator_one_btn -> calculatorNumberButton("1")
+            R.id.calculator_two_btn -> calculatorNumberButton("2")
+            R.id.calculator_three_btn -> calculatorNumberButton("3")
+            R.id.calculator_four_btn -> calculatorNumberButton("4")
+            R.id.calculator_five_btn -> calculatorNumberButton("5")
+            R.id.calculator_six_btn -> calculatorNumberButton("6")
+            R.id.calculator_seven_btn -> calculatorNumberButton("7")
+            R.id.calculator_eight_btn -> calculatorNumberButton("8")
+            R.id.calculator_nine_btn -> calculatorNumberButton("9")
+            R.id.calculator_plus_btn -> calculatorOperatorButton("+")
+            R.id.calculator_minus_btn -> calculatorOperatorButton("-")
+            R.id.calculator_multi_btn -> calculatorOperatorButton("*")
+            R.id.calculator_divider_btn -> calculatorOperatorButton("/")
+            R.id.calculator_modulo_btn -> calculatorOperatorButton("%")
+        }
+    }
+
+    private fun calculatorNumberButton(number: String) {
 
     }
 
-    fun calculatorClearButton(v: View) {
+    private fun calculatorOperatorButton(operator: String) {
+
+    }
+
+    private fun calculatorClearButton(v: View) {
+
+    }
+
+    private fun calculatorResultButton(v: View) {
 
     }
 
     fun calculatorHistoryButton(v: View) {
-
-    }
-
-    fun calculatorResultButton(v: View) {
 
     }
 }
