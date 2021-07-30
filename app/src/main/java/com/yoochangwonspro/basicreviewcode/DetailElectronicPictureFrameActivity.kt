408bc068bfbd80d4fcac2abf41d8ad5831e97ce0
CN <ckddnjs554@naver.com>
2021-07-30 19:36:13 +0900

    Feat : DetailElectronicPictureFrameActivity View 전역변수 작성

--- app/src/main/java/com/yoochangwonspro/basicreviewcode/DetailElectronicPictureFrameActivity.kt
+++ app/src/main/java/com/yoochangwonspro/basicreviewcode/DetailElectronicPictureFrameActivity.kt
@@ -2,8 +2,20 @@ package com.yoochangwonspro.basicreviewcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class DetailElectronicPictureFrameActivity : AppCompatActivity() {
+
+    private val electronicBackGroundImageView: ImageView by lazy {
+        findViewById(R.id.electronic_picture_background_image_view)
+    }
+
+    private val electronicPictureImageView: ImageView by lazy {
+        findViewById(R.id.electronic_picture_image_view)
+    }
+
+    private var currentPosition = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dtail_electronic_picture_frame)
