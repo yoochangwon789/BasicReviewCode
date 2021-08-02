891e6bbdda774f23e7a7a3ea9e5b577dd2f0732b
CN <ckddnjs554@naver.com>
2021-08-02 23:48:30 +0900

    Feat : MyPracticeLottoProject 디자인 구성 작업

---
+++ app/src/main/java/com/yoochangwonspro/basicreviewcode/MyPracticeLottoProject.kt
@@ -0,0 +1,11 @@
+package com.yoochangwonspro.basicreviewcode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyPracticeLottoProject : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_practice_lotto_project)
+    }
+}
