bfc23077d798867a3108344e7d23de9933f6ecf0
CN <ckddnjs554@naver.com>
2021-08-07 16:25:58 +0900

    Feat : BasicWebBrowserActivity WebTopBar 의 필요한 버튼 모양및 배경 추가 작업

---
+++ app/src/main/java/com/yoochangwonspro/basicreviewcode/BasicWebBrowserActivity.kt
@@ -0,0 +1,11 @@
+package com.yoochangwonspro.basicreviewcode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BasicWebBrowserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basic_web_browser)
+    }
+}
